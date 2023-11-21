52c5d67e844c4ed6245a9ed3b08ab934d3d42198
Lea Yeh <lyeh@c2r2p6.42vienna.com>
2023-11-21 17:37:03 +0100

    fix: Fix norm issue

--- source/mandatory/object/background.c
+++ source/mandatory/object/background.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 12:56:46 by lyeh              #+#    #+#             */
-/*   Updated: 2023/11/21 14:24:33 by lyeh             ###   ########.fr       */
+/*   Updated: 2023/11/21 17:36:40 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void	draw_background(t_game *game)
 		col = 0;
 		while (col < game->map->width)
 		{
-			draw_block(game, game->s_background[0], row * BLOC_PX, col * BLOC_PX);
+			draw_block(
+				game, game->s_background[0], row * BLOC_PX, col * BLOC_PX);
 			col++;
 		}
 		row++;
